9c4f9247f802a175516d45b087bf3fbec3149637
Alejandro Lembke Barrientos <aleleba@hotmail.com>
2023-08-25 16:29:22 +0000

    Fixing entrypoint.sh Updating to version 1.0.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 exec [[ -z "${HOME_USER}" ]] && adduser --disabled-password --gecos "" vscode \
     echo 'vscode ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers \
     || adduser --disabled-password --gecos "" ${HOME_USER} \
