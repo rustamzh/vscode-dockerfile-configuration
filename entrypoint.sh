1e044e4959fab6b8c17a6a454fa558fff2b43e59
Alejandro Lembke Barrientos <aleleba@hotmail.com>
2023-08-25 17:18:17 +0000

    Trying to fix entrypoint.sh Updating to version 1.0.4

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-[[ -z "${HOME_USER}" ]] && adduser --disabled-password --gecos "" vscode \
-    echo 'vscode ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers \
-    || adduser --disabled-password --gecos "" ${HOME_USER} \
-    echo '${HOME_USER} ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+[[ -z "${HOME_USER}" ]] && (adduser --disabled-password --gecos "" vscode && echo 'vscode ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers) \
+    || (adduser --disabled-password --gecos "" ${HOME_USER} && echo '${HOME_USER} ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers)
 [[ -z "${VSCODE_TUNNEL_NAME}" ]] && code tunnel --accept-server-license-terms || code tunnel --accept-server-license-terms --name ${VSCODE_TUNNEL_NAME}
