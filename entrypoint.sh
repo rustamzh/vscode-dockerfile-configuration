3dc1c03a0f6b0dcc98a87c1790b5e00bbf26ef7a
Alejandro Lembke Barrientos <aleleba@hotmail.com>
2023-08-27 00:29:06 +0000

    Trying to fix entrypoint.sh Updating to version 1.0.12

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,8 @@ addgroup nonroot
 adduser --disabled-password --gecos "" ${HOME_USER}
 echo "${HOME_USER} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-su - ${HOME_USER}
-
 if [[ -z "${VSCODE_TUNNEL_NAME}" ]]; then
-    code tunnel --accept-server-license-terms
+    sudo su - ${HOME_USER} && code tunnel --accept-server-license-terms
 else
-    code tunnel --accept-server-license-terms --name ${VSCODE_TUNNEL_NAME}
+    sudo su - ${HOME_USER} && code tunnel --accept-server-license-terms --name ${VSCODE_TUNNEL_NAME}
 fi
